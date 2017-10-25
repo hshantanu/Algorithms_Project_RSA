a10b0093a9733a4d04d228ebb188eb65fe6700cb
hshantanu <2hshantanu@gmail.com>
2017-10-25 13:33:31 -0400

    Sample code to generate Primes. It uses Miller-Rabin Primality Test. Will update this a bit later on.

---
+++ TestPrime.py
@@ -0,0 +1,41 @@
+from random import randrange, getrandbits
+from itertools import repeat
+
+ #Reference: https://jhafranco.com/2012/01/29/rsa-implementation-in-python/
+
+def getPrime(n):
+    """Get a n-bit pseudo-random prime"""
+    def isProbablePrime(n, t = 1):
+        """Miller-Rabin primality test"""
+        def isComposite(a):
+            """Check if n is composite"""
+            if pow(a, d, n) == 1:
+                return False
+            for i in range(s):
+                if pow(a, 2 ** i * d, n) == n - 1:
+                    return False
+            return True
+
+        assert n > 0
+        if n < 3:
+            return [False, False, True][n]
+        elif not n & 1:
+            return False
+        else:
+            s, d = 0, n - 1
+            while not d & 1:
+                s += 1
+                d >>= 1
+
+        for _ in repeat(None, t):
+            if isComposite(randrange(2, n)):
+                return False
+        return True   
+     
+    p = getrandbits(n)
+    while not isProbablePrime(p):
+        p = getrandbits(n)
+    return p
+
+p1 = getPrime(128)
+print p1
