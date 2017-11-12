d8810dce12aae7d8e3463c12052e01b940d3bb05
spizler <aspizler@gmail.com>
2017-11-11 21:13:20 -0500

    Added prime number base generator and random message generator.

--- rns.py
+++ rns.py
@@ -1,52 +1,36 @@
 import math
+import random
+from Crypto.Util import number
 
 class ResidueNumberSystem(object):
 	"""
 	Holds the RNS base and performs operations on numbers
 	"""
 
-	def __init__(self, method="mod", base=0):
+	def __init__(self, method, numBits, numBasis):
 		""" Set the RNS base vector. All base elements must be coprime """
-		if (method == "mod"):
-			if (base == 0):
-				self.base = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
-			else:
-				self.base = base
-		elif (method == "blocks"):
-			if (base == 0):
-				self.base = self.get32bitbase();
-			else:
-				raise ValueError("Only use default blocks with method='blocks'")
-		else:
-			raise ValueError("method must be either 'mod' or 'blocks'")
-
+		self.base = self.getBase(numBits, numBasis)
+		self.numBits = numBits
 		self.method = method
-		self.maxintlen = math.log(self.maxint(), 2)
 
-	def get32bitbase(self):
-		""" returns a 31 bit base of size 10 """
-		K = [5, 17, 65, 99, 107, 135, 153, 185, 209, 267]
-		n = 32;
-		base = []
-		for k in K:
-			base.append(pow(2,n) - k)
-		return base
+	def getBase(self, numBits, numBasis):
+		primeNums = []
+		for n in range(numBasis):
+			primeNum = number.getPrime(numBits)
+			while primeNum in primeNums:
+				primeNum = number.getPrime(numBits)
+			primeNums.append(primeNum)
+		return primeNums
 
-
-	def maxint(self):
+	def maxbits(self):
 		""" returns the max value that can be stored in the rns """
 		if (self.method == "mod"):
 			result = 1
 			for b in self.base:
 				result *= b
-			return result - 1
+			return math.floor(math.log(result, 2))
 		else:
-			length = 0
-			for b in self.base:
-				length += math.floor(math.log(b, 2))
-			result = pow(2,length)
-			return result
-
+			return (self.numBits - 1) * len(self.base)
 
 	def int2rns(self, number):
 		""" return rns representation of an int """
@@ -61,18 +45,19 @@ class ResidueNumberSystem(object):
 
 			# check length
 			strlen = len(binstr)
-			if strlen > self.maxintlen:
+			if strlen > self.maxbits():
 				raise ValueError("Integer exceeds max rns integer")
 
 			# padd to length
-			padlen = self.maxintlen - strlen
+			padlen = self.maxbits() - strlen
 			pad = "0" * int(padlen)
 			binstr += pad
 
+
 			result = []
 			for i in range(len(self.base)):
-				valstr = binstr[31*i:31*(i+1)]
-				result.append(int(valstr))
+				valstr = binstr[(self.numBits - 1)*i : (self.numBits - 1)*(i+1)]
+				result.append(long(valstr, 2))
 		
 		return result
 
@@ -82,34 +67,22 @@ class ResidueNumberSystem(object):
 			decmsg = int(hexmsg, 16)
 			return self.int2rns(decmsg)
 
-
+	def randomMessage(self, n):
+		return random.getrandbits(n)
 
 def main():
-	rns = ResidueNumberSystem("blocks")
-	print "base: ", rns.base
-	print "max:", rns.maxint()
+	bits = 2**10
+	print "bits:", bits
+	rns = ResidueNumberSystem("blocks", bits, 1)
+	print "Max message bits:", rns.maxbits()
 	
-	M = "Hello World! How are you? Good man! :)"
-	print "message(", M, ":", rns.msg2rns(M)
+	M = rns.randomMessage(bits - 1)
 
-	# a = pow(2, 100) + pow(2, 200)
-	# print "rns(", a, "):", rns.int2rns(a)
-
-
-
-	# print "lg(max):", math.log(rns.maxint(), 2) # max is a 310 bit message
-	# print "max msg length", math.log(rns.maxint(), 2)/8, "bytes"
-
-	# rns = ResidueNumberSystem("mod")
-	# print "base: ", rns.base
-
-	# max = rns.maxint()
-	# print "max: ", max
-
-	# a = 23
-	# A = rns.int2rns(23)
-	# print "rns(", a, "): ", A
-
+	# M = "Hello World! How are you? Good man! :)"
+	print "MsgInt: ", M
+	print "Base:  ", rns.base
+	# print "Msg: ", rns.msg2rns(M)
+	print "MsgRNS:", rns.int2rns(M)
 
 if __name__ == '__main__':
 	main()
