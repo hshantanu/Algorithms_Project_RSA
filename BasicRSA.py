8d119d2721724ea13bb0598829c7c6179d2fe1b2
hshantanu <2hshantanu@gmail.com>
2017-11-15 00:05:38 -0500

    Updated the code to add the regularRSATrial(). It has a bug, though.

--- BasicRSA.py
+++ BasicRSA.py
@@ -5,6 +5,7 @@ from itertools import repeat
 from functools import reduce
 from Crypto.Util import number
 import sys
+from time import time
 
 def getPrime(numBits):
     primeNum = number.getPrime(numBits)
@@ -32,7 +33,6 @@ def inv(p, q):
 def generateRSAParameters(bits):
     #Generate Public and Private Keys
     #65537 is the largest known prime number of the form {\displaystyle 2^{2^{n}}+1} 2^{{2^{{n}}}}+1 ( {\displaystyle n=4} n=4).
-    val_list = []
     p = getPrime(bits)
     q = getPrime(bits)
     var = (p - 1) * (q - 1)
@@ -41,8 +41,65 @@ def generateRSAParameters(bits):
         e, d = 3, inv(3, var)
     else:
         e, d = 65537, inv(65537, var)
-    val_list.append([p, q, n, e, d])
+    val_list = [p, q, n, e, d]
     return val_list
 
-for bits in [64]:    
+def encrypt(M, e, n):
+    #Return length (in bytes) of modulus
+    size = len("{:02x}".format(n)) // 2
+    output = []
+    while M:
+        nbytes = min(len(M), size - 1)
+        #Converting text to integer
+        int_text = reduce(lambda x, y : (x << 8) + y, map(ord, M[:nbytes]))        
+        assert int_text < n
+        power = pow(int_text, e, n)
+        #Converting an integer to a list of small integers
+        output += [(power >> j) & 0xff for j in reversed(range(0, (size + 2) << 3, 8))]
+        M = M[size:]
+    return output
+
+def decrypt(CT, d, p, q, n):
+    #Decryption using Chinese Remainder Theorem
+    size = len("{:02x}".format(n)) // 2
+    output = ""
+    while CT:
+        #Convert a list of integers into an integer
+        integer = reduce(lambda x, y : (x << 8) + y, CT[:size + 2])
+        assert integer < n
+        m1 = pow(integer, d % (p - 1), p)
+        m2 = pow(integer, d % (q - 1), q)
+        h = (inv(q, p) * (m1 - m2)) % p
+        res = m2 + h * q
+        #Convert an integer into a text string
+        text = "".join([chr((res >> j) & 0xff) for j in reversed(range(0, size << 3, 8))])
+        output += text.lstrip("\x00")  
+        CT = CT[size + 2:]
+    return output
+
+def printCipher(CT):
+    for index, elem in enumerate(CT):
+            sys.stdout.write(str(elem))
+    print ""
+
+def regularRSATrial(val_list):
+    #val_list = [p, q, n, e, d]
+    t1 = time()
+    M = "This message is a small test."
+    p = val_list[0]
+    q = val_list[1]
+    n = val_list[2]
+    e = val_list[3]
+    d = val_list[4]
+    CT = encrypt(M, e, n)
+    print "Ciphertext: "
+    printCipher(CT)
+    DT = decrypt(CT, d, p, q, n)
+    print "Regular Text: "
+    print DT
+    t2 = time()
+    print "Time: {:0.3f} s".format(round(t2 - t1, 3))
+
+for bits in [128, 512]:    
     val_list = generateRSAParameters(bits) #p,q,n,e,d
+    regularRSATrial(val_list)
