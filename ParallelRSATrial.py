232c50b739da419f8247e95982dfb5917f3afc1e
spizler <aspizler@gmail.com>
2017-11-20 15:50:22 -0500

    changed

--- ParallelRSATrial.py
+++ ParallelRSATrial.py
@@ -8,12 +8,11 @@ class ParallelRSA(object):
 	Performs trials of RSA
 	"""
 
-	def ParallelRSATrial(self, val_list, M, numProcessors, numbits):
-		p = val_list[0]
-		q = val_list[1]
-		n = val_list[2]
-		e = val_list[3]
-		d = val_list[4]
+	def ParallelRSATrial(self, keys, M, numProcessors, numbits):
+		[pubkey prikey] = keys
+		n = prikey[0]
+		e = prikey[1]
+		d = prikey[2]
 
 		time1 = time.time()
 		rns = RNS.ResidueNumberSystem()
@@ -25,13 +24,21 @@ class ParallelRSA(object):
 
 		time2 = 0
 
+		print "e:", e
+		print "d:", d
+
 		for i in range(len(B)):
 		# for m in Mrns:
 			m = Mrns[i]
 			b = B[i]
 			start = time.time()
+			t1 = time.time()
 			c = MM.MontogomeryExponentiation(m, e, n)
+			t2 = time.time()
 			Mrns_d.append(MM.MontogomeryExponentiation(c, d, n))
+			t3 = time.time()
+			print "t_e:", t2 - t1
+			print "t_d:", t3 - t2
 			stop = time.time()
 			time2 = max(time2, stop - start)
 
@@ -40,7 +47,7 @@ class ParallelRSA(object):
 		while (Md[-1].encode("hex") == "00"):
 			Md = Md[0:-1]
 		time3 = time.time() - time3
-		return [M==Md, time1+time2+time3]
+		return [M==Md, time2]
 
 	def bitlength(self, n):
 		binstr = str(bin(n))[2:]
